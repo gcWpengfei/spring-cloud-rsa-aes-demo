3dcae0ee2c033f244d7cdfdd35bafcedcb473680
gcWpengfei <1304070598@qq.com>
2018-05-25 16:15:47 +0800

    拦截器

---
+++ aes-rsa-client/src/main/java/com/wpf/interceptor/ParamInterceptor.java
@@ -0,0 +1,59 @@
+package com.wpf.interceptor;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+
+
+
+public class ParamInterceptor implements HandlerInterceptor {
+
+    private Logger LOG = Logger.getLogger(ParamInterceptor.class);
+
+    @SuppressWarnings("rawtypes")
+	@Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+
+        String token = httpServletRequest.getHeader("token");
+        LOG.info(token);
+       
+		Map requestParams = httpServletRequest.getParameterMap();
+    	for (Iterator iter = requestParams.keySet().iterator(); iter.hasNext();) {
+    		String name = (String) iter.next();
+    		String[] values = (String[]) requestParams.get(name);
+    		String valueStr = "";
+    		for (int i = 0; i < values.length; i++) {
+    			valueStr = (i == values.length - 1) ? valueStr + values[i]
+    					: valueStr + values[i] + ",";
+    		}
+    		//乱码解决，这段代码在出现乱码时使用。如果mysign和sign不相等也可以使用这段代码转化
+    		//valueStr = new String(valueStr.getBytes("ISO-8859-1"), "gbk");
+    		//params.put(name, valueStr);
+    		
+    		LOG.info(valueStr);
+    		
+    	}
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
---
+++ aes-rsa-client/src/main/java/com/wpf/interceptor/ParamInterceptor2.java
@@ -0,0 +1,60 @@
+/*package com.wpf.interceptor;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+
+
+
+public class ParamInterceptor2 implements HandlerInterceptor {
+
+
+
+    private Logger LOG = Logger.getLogger(ParamInterceptor2.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+
+        String token = httpServletRequest.getHeader("token");
+        //LOG.info(token)
+       
+        if(!"aOXnjYZyn==".equals(token)){
+            PrintWriter writer = null;
+            httpServletResponse.setCharacterEncoding("UTF-8");
+            httpServletResponse.setContentType("text/html; charset=utf-8");
+            try {
+                writer = httpServletResponse.getWriter();
+                String error = "token信息有误";
+                writer.print(error);
+                return false;
+
+            } catch (IOException e) {
+                LOG.error("response error",e);
+            } finally {
+                if (writer != null)
+                    writer.close();
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
+*/
