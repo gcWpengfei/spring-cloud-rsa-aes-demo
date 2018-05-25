46907febdf8541638c61820ee55cc1e593a37805
gcWpengfei <1304070598@qq.com>
2018-05-25 10:54:49 +0800

    test测试提交

--- aes-rsa-client/src/main/java/com/wpf/controller/ClientController.java
+++ aes-rsa-client/src/main/java/com/wpf/controller/ClientController.java
@@ -21,8 +21,8 @@ public class ClientController {
 	private TestFeign testFeign;
 
 	// 客户端 将原始私钥的转换成PKCS8格式的私钥11111111111111111111111111111111111111
-	//akjks
-		public static final String clientPrivateKey = "MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAM9mfM/BTr2IPPtB" + 
+	//akjks  1111
+		public static   final String clientPrivateKey = "MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAM9mfM/BTr2IPPtB" + 
 				"GyDYo1BkDbdBgfgDhvY+dFB22hF5t5aG1g1KW6CdQog7INM95YI3+vHMdlPvmkd8" + 
 				"vEjcBlLkd0EWvRIV7wv5upGO8TJ+vYor4EaoR7sx47996pm/TZD5jSWGesRrhTI4" + 
 				"CkbVGe4l06rfBZ8PRO2XKNpqGR8/AgMBAAECgYAkD5rV+LN7KuwHd7uCy4gh2zOz" + 
