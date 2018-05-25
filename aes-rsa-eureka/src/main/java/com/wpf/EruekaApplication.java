4a3f6f3efb88509b05ecc518a5c7baebc54f86d8
gcWpengfei <1304070598@qq.com>
2018-05-25 16:38:48 +0800

    commit

--- aes-rsa-eureka/src/main/java/com/wpf/EruekaApplication.java
+++ aes-rsa-eureka/src/main/java/com/wpf/EruekaApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class EruekaApplication {
 
-    //
+    // 启动器
     public static void main(String[] args){
         SpringApplication.run(EruekaApplication.class,args);
     }
