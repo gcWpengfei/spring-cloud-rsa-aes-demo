cd633155103e227896e0a69e5e81cbcf5213999d
gcWpengfei <1304070598@qq.com>
2018-06-01 18:38:19 +0800

    添加了springboot-admin

---
+++ springboot-admin/src/main/java/aesrsa/springbootadmin/SpringbootAdminApplication.java
@@ -0,0 +1,16 @@
+package aesrsa.springbootadmin;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableAdminServer
+@EnableEurekaClient
+@SpringBootApplication
+public class SpringbootAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAdminApplication.class, args);
+	}
+}
