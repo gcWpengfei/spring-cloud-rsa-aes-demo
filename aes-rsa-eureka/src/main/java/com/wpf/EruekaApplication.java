a31a982b76366850b4a99af82e06a62d3856b565
gcWpengfei <1304070598@qq.com>
2018-06-05 18:41:26 +0800

    添加的邮箱服务通知

--- aes-rsa-eureka/src/main/java/com/wpf/EruekaApplication.java
+++ aes-rsa-eureka/src/main/java/com/wpf/EruekaApplication.java
@@ -2,10 +2,12 @@ package com.wpf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+//@EnableDiscoveryClient
 public class EruekaApplication {
 
     // 启动器
