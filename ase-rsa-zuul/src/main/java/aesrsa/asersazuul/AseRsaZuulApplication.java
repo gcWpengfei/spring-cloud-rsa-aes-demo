fed4b601973032393feb0b8b483dc91de93a1b3d
gcWpengfei <1304070598@qq.com>
2018-06-06 18:05:24 +0800

    zuul网关路由分配

---
+++ ase-rsa-zuul/src/main/java/aesrsa/asersazuul/AseRsaZuulApplication.java
@@ -0,0 +1,16 @@
+package aesrsa.asersazuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class AseRsaZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AseRsaZuulApplication.class, args);
+	}
+}
---
+++ ase-rsa-zuul/src/test/java/aesrsa/asersazuul/AseRsaZuulApplicationTests.java
@@ -0,0 +1,16 @@
+package aesrsa.asersazuul;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AseRsaZuulApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
