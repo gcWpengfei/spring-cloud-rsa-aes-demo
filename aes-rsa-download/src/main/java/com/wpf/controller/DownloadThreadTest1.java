ac057c660cd194f0a6dae445ff9d345f96f9a9d1
gcWpengfei <1304070598@qq.com>
2018-06-12 17:13:07 +0800

    邮件服务，断点续传功能

--- aes-rsa-download/src/main/java/com/wpf/controller/DownloadThreadTest1.java
+++ aes-rsa-download/src/main/java/com/wpf/controller/DownloadThreadTest1.java
@@ -1,6 +1,10 @@
-package com.wpf.util.down;
+package com.wpf.controller;
 
+import org.apache.commons.lang.exception.ExceptionUtils;
+import org.apache.log4j.Logger;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -8,75 +12,9 @@ import java.io.RandomAccessFile;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-/**
- * 多线程下载和断点续传
- *
- * @Author: wpf
- * @Date: 16:42 2018/6/12
- * @Description: 
- * @param  * @param null  
- * @return   
- */
-public class MutiDownloadTest1 {
-    private static final int    THREAD_COUNT    = 5;
-    private static final String    DOWNLOAD_URL    = "http://down.360safe.com/se/360se9.1.0.426.exe";
-    private static final String    fileName        = "F:\\1\\test\\2/360.exe";
-     static final String    filePath        = "F:\\1\\test\\2/";
+public class DownloadThreadTest1 implements Runnable {
 
-    public static void main(String[] args) {
-
-        long fileSize = 0;
-        HttpURLConnection connection = null;
-        try {
-            connection = (HttpURLConnection)new URL(DOWNLOAD_URL).openConnection();
-            connection.setRequestMethod("GET");
-            connection.setConnectTimeout(8000);
-            connection.setReadTimeout(8000);
-
-            /**
-             * 当请求成功时,返回http状态码200
-             */
-            if (connection.getResponseCode() == 200) {
-                /**
-                 * 打开一个RandomAccessFile文件,打开方式为读写(rw)
-                 * setLength是先在存储设备占用一块空间,防止下载到一半空间不足
-                 */
-                RandomAccessFile randomAccessFile = new RandomAccessFile(fileName , "rw");
-                fileSize = connection.getContentLength();
-                randomAccessFile.setLength(fileSize);
-                randomAccessFile.close();
-
-                long eachSize = fileSize/THREAD_COUNT;
-                for(int i=0; i<THREAD_COUNT; i++){
-                    long startIndex = i*eachSize;
-                    long endIndex = (i+1)*eachSize - 1;
-                    /**
-                     * 当时最后一个线程的时候,endIndex的值就由文件大小
-                     */
-                    if(i == THREAD_COUNT - 1){
-                        endIndex = fileSize;
-                    }
-
-                    Runnable runnable = new DownloadThreadTest1(DOWNLOAD_URL,fileName,i, startIndex, endIndex);
-                    new Thread(runnable).start();
-
-                }
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            // log.info(ExceptionUtils.getFullStackTrace(e));
-        } finally {
-            if(null != connection){
-                connection.disconnect();
-            }
-        }
-    }
-}
-
-class DownloadThreadTest1 implements Runnable {
-
-    // private static final Logger log = Logger.getLogger(DownloadThreadTest.class);
+    private static final Logger log = Logger.getLogger(DownloadThreadTest1.class);
 
     private String url;
     private String fileName;
@@ -89,14 +27,19 @@ class DownloadThreadTest1 implements Runnable {
     private HttpURLConnection httpURLConnection;
     private RandomAccessFile randomAccessFile;
     private InputStream inputStream;
+    private HttpServletRequest request;
+    private HttpServletResponse response;
 
-    public DownloadThreadTest1(String url, String fileName, int threadId, long startIndex, long endIndex) {
+    DownloadThreadTest1(HttpServletRequest request, HttpServletResponse response,
+                        String url, String fileName, int threadId, long startIndex, long endIndex) {
         super();
         this.url = url;
         this.fileName = fileName;
         this.threadId = threadId;
         this.startIndex = startIndex;
         this.endIndex = endIndex;
+        this.request = request;
+        this.response = response;
 
     }
 
@@ -106,7 +49,7 @@ class DownloadThreadTest1 implements Runnable {
         /*
              * 查看临时文件
              */
-        File downThreadFile = new File(MutiDownloadTest1.filePath, "wpf_thread_"+threadId+".dt");
+        File downThreadFile = new File("F:\\1\\test\\2/", "wpf_thread_"+threadId+".dt");
         try {
 
             if(downThreadFile.exists()){
@@ -115,6 +58,7 @@ class DownloadThreadTest1 implements Runnable {
                 if(null == startIndex_str || "".equals(startIndex_str)){
                 } else {
                     this.startIndex = Long.parseLong(startIndex_str) - 1; // //设置下载起点
+                    log.info(this.startIndex);
                 }
             } else {
                 downThreadStream = new RandomAccessFile(downThreadFile, "rwd");
@@ -147,7 +91,7 @@ class DownloadThreadTest1 implements Runnable {
                 int total = 0;
                 while((len = inputStream.read(bytes)) != -1){
                     total += len;
-                    //log.info("线程" + threadId + ":" + total);
+                    log.info("线程" + threadId + ":" + total);
                     System.out.println("线程: " +  threadId + ":" + total);
                     randomAccessFile.write(bytes, 0, len);
 
@@ -162,9 +106,10 @@ class DownloadThreadTest1 implements Runnable {
             }
         } catch (IOException e) {
             e.printStackTrace();
-            // log.info(ExceptionUtils.getFullStackTrace(e));
+            log.info(ExceptionUtils.getFullStackTrace(e));
         } finally {
             try {
+                cleanTemp(downThreadFile);
                 if(null != downThreadStream){
                     downThreadStream.close();
 
@@ -178,10 +123,10 @@ class DownloadThreadTest1 implements Runnable {
                 if(null != randomAccessFile){
                     randomAccessFile.close();
                 }
-                cleanTemp(downThreadFile);
+
             } catch (Exception e) {
                 e.printStackTrace();
-                //log.info(ExceptionUtils.getFullStackTrace(e));
+                log.info(ExceptionUtils.getFullStackTrace(e));
             }
         }
     }
--- aes-rsa-download/src/main/java/com/wpf/controller/TestController.java
+++ aes-rsa-download/src/main/java/com/wpf/controller/TestController.java
@@ -1,17 +1,80 @@
 package com.wpf.controller;
 
+import org.apache.commons.lang.exception.ExceptionUtils;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @Controller
 @RequestMapping("/")
 public class TestController {
 
+    private static final Logger log = Logger.getLogger(DownloadThreadTest1.class);
+    private static final int    THREAD_COUNT    = 5;
+    private static final String    DOWNLOAD_URL    = "http://down.360safe.com/se/360se9.1.0.426.exe";
+    private static final String    fileName        = "F:\\1\\test\\2/360.exe";
+    static final String    filePath        = "F:\\1\\test\\2/";
+
+    @GetMapping("/download")
     public void download(HttpServletRequest request, HttpServletResponse response){
 
-        //response.getOutputStream()
+        long fileSize = 0;
+        HttpURLConnection connection = null;
+        try {
+            connection = (HttpURLConnection)new URL(DOWNLOAD_URL).openConnection();
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(8000);
+            connection.setReadTimeout(8000);
+            log.info(connection);
+
+            /**
+             * 当请求成功时,返回http状态码200
+             */
+            if (connection.getResponseCode() == 200) {
+                /**
+                 * 打开一个RandomAccessFile文件,打开方式为读写(rw)
+                 * setLength是先在存储设备占用一块空间,防止下载到一半空间不足
+                 */
+                RandomAccessFile randomAccessFile = new RandomAccessFile(fileName , "rw");
+                fileSize = connection.getContentLength();
+                randomAccessFile.setLength(fileSize);
+                randomAccessFile.close();
+
+                long eachSize = fileSize/THREAD_COUNT;
+                log.info(eachSize);
+                for(int i=0; i<THREAD_COUNT; i++){
+                    long startIndex = i*eachSize;
+                    long endIndex = (i+1)*eachSize - 1;
+                    log.info(startIndex);
+                    log.info(endIndex);
+                    /**
+                     * 当时最后一个线程的时候,endIndex的值就由文件大小
+                     */
+                    if(i == THREAD_COUNT - 1){
+                        endIndex = fileSize;
+                    }
+
+                    Runnable runnable = new DownloadThreadTest1(request, response,DOWNLOAD_URL,fileName,i, startIndex, endIndex);
+                    new Thread(runnable).start();
+
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            log.info(ExceptionUtils.getFullStackTrace(e));
+        } finally {
+            if(null != connection){
+                connection.disconnect();
+            }
+        }
     }
 }
