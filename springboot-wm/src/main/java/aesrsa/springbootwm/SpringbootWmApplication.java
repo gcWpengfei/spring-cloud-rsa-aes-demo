f64e51146f276317860766821c02758bd7cab40e
gcWpengfei <1304070598@qq.com>
2018-06-23 10:56:40 +0800

    爬虫

---
+++ springboot-wm/src/main/java/aesrsa/springbootwm/SpringbootWmApplication.java
@@ -0,0 +1,12 @@
+package aesrsa.springbootwm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWmApplication.class, args);
+	}
+}
